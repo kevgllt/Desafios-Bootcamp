8a9f0921ddeed238e3ad40bd62adc288b37cd2f4
kev gullyt <60349527+kevgllt@users.noreply.github.com>
2020-08-25 22:26:32 -0300

    Update vetores-objetos.js

--- FASE-02/desafio-1-2/vetores-objetos.js
+++ FASE-02/desafio-1-2/vetores-objetos.js
@@ -1,1 +1,12 @@
-console.log('hello world')
+const usuario = {
+    nome: 'Toshio',
+    idade: 7,
+    tecnologias: [
+        {nome: 'Javascript', especialidade: 'Web/mobile'},
+        {nome: 'C++', especialidade: 'Desktop'},
+        {nome: 'Python', especialidade: 'Data Science'}
+    ]
+    
+}
+
+console.log(`O usuario ${usuario.nome} tem ${usuario.idade} anos e usa a tecnologia ${usuario.tecnologias[0].nome} com especialidade em ${usuario.tecnologias[0].especialidade}`)
