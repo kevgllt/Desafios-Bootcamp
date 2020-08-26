6fe6a17c9b6f79bf99bc32a9292031b51f2074ae
kev gullyt <60349527+kevgllt@users.noreply.github.com>
2020-08-25 21:40:01 -0300

    desafio resolvido

--- FASE-02/desafio-1-2/construcao-objetos.js
+++ FASE-02/desafio-1-2/construcao-objetos.js
@@ -1,1 +1,11 @@
-console.log('hello world');
+const usuario = {
+    nome: 'Poul',
+    empresa: {
+        nome: 'Rockeseat',
+        rua: 'Rua Guilherme Gembala',
+        numero: 260
+    }
+}
+
+console.log(`A empresa ${usuario.empresa.nome} está localizada em ${usuario.empresa.rua}, ${usuario.empresa.numero}.`)
+
